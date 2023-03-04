ac034c475f91811078b8283ed4365a28a460aa2f
NguyenAnhKietUIT <20521498@gm.uit.edu.vn>
2023-03-05 01:33:37 +0700

    solution 2 for two sum javascript

--- Javascript/TwoSum.js
+++ Javascript/TwoSum.js
@@ -1,4 +1,5 @@
 /**
+ * Solution 1
  * @param {number[]} nums
  * @param {number} target
  * @return {number[]}
@@ -12,4 +13,25 @@ var twoSum = function(nums, target) {
         if (result.hasOwnProperty(temp)) return [result[temp], i]
         result[nums[i]] = i
     }
+};
+
+/**
+ * Solution 2
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let store = new Map();
+    let result = [];
+
+    nums.forEach((item, index) => {
+        if (store.has(item)) {
+            result.push(store.get(item), index);
+        } else {
+            let diff= target -item;
+            store.set(diff, index);
+        }
+    })
+    return result;
 };
